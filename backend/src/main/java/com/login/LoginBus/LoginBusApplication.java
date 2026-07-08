b6d195f6443da9d50cadf2309bf359d68e8af6e6
Kabera192 <ckabera6@gmail.com>
2026-07-08 14:19:09 +0200

    chore: add .env file and a more robust gitignore

--- backend/src/main/java/com/login/LoginBus/LoginBusApplication.java
+++ backend/src/main/java/com/login/LoginBus/LoginBusApplication.java
@@ -3,10 +3,20 @@ package com.login.LoginBus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class LoginBusApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USER", dotenv.get("DB_USER"));
+        System.setProperty("DB_PWD", dotenv.get("DB_PWD"));
+		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+        System.setProperty("JWT_ISSUER", dotenv.get("JWT_ISSUER"));
+
 		SpringApplication.run(LoginBusApplication.class, args);
 	}
 
